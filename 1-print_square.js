80f48bd982e8c3643a59a97bac2917879bfd866f
Edwin Bwambale <bwambaleedwin03@gmail.com>
2024-04-04 12:42:54 -0400

    peinting a square of a definite size from a users's input

--- 1-print_square.js
+++ 1-print_square.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
 /*
-print a square with te character #
-the size of the square ust be the first argument of the script
-of the program.
+print a square with the character #
+the size of the square must be the first argument of the script
 */
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
@@ -11,10 +10,15 @@ if (process.argv.length <= 2) {
     process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1);
 }
-size = parseInt(process.argv[2], 16)
+let size = parseInt(process.argv[2], 10); // Assuming you want a decimal input
+
+if (isNaN(size)) {
+    process.stderr.write("The size must be a number\n");
+    process.exit(1);
+}
 
 for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; i++) {
+    for (let j = 0; j < size; j++) {
         process.stdout.write('#');
     }
     process.stdout.write('\n');
