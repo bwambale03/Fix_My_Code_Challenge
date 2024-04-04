0f7720da1f8aa57725f5da462e24ecc7b0d8d198
Edwin Bwambale <bwambaleedwin03@gmail.com>
2024-04-04 13:30:28 -0400

    sorting integer arguments (ascending)

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -2,28 +2,14 @@
 # 
 # Sort integer arguments (ascending)
 ##
-result = []
-ARGV.each do |arg|
-    # skip if not integer
-    next if arg !~ /^-?\d+$/
-    result << arg.to_i
-
-    # convert to integer
-    i_arg = arg.to_i
-
-    # insert result at the right positions
-    is_inserted = false
-    i=0
-    l=result.size
-    while !is_inserted && i<l do
-        if result[i] < i_arg
-            i += 1
-        else
-            result.insert(i-1, i_arg)
-            is_inserted = true
-            break
-        end
-    end
-    result << i_arg if !is_inserted
-end
-puts result
+result = ARGV.filter_map do |arg|
+    # Convert to integer if it's a valid integer string, otherwise return nil
+    arg.to_i if arg.match?(/^[-]?\d+$/)
+  end
+  
+  # Sort the array of integers in ascending order
+  result.sort!
+  
+  # Print each sorted integer on a new line
+  puts result
+  
